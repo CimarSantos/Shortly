22541377a82a4aab83b46dc6122a4c1d28792f3c
CimarSantos <francimarlsantos@outlook.com>
2023-03-03 15:16:43 -0300

    Feat: Rota /users/me

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,32 +1,41 @@
+import { db } from "../database/database.js";
 import { STATUS_CODE } from "../enums/statusCode.js";
-import * as userRepository from "../repositories/usersRepository.js";
 
 async function getDataUsers(req, res) {
   const { userId } = res.locals;
 
   try {
-    const { rows: userData } = await userRepository.selectUserData(userId);
+    const [usersUrls, visits, userinfo] = await Promise.all([
+      db.query(
+        'SELECT id, "shortUrl", url, visitcount FROM urls WHERE userid = $1;',
+        [userId]
+      ),
+      db.query("SELECT SUM(visitcount) FROM urls WHERE userid = $1;", [userId]),
+      db.query("SELECT id, name FROM users WHERE id = $1;", [userId]),
+    ]);
 
-    if (userData.length === 0) {
-      return res.sendStatus(STATUS_CODE.NOT_FOUND);
-    }
+    const myUrls = {
+      id: userinfo.rows[0]?.id,
+      name: userinfo.rows[0]?.name,
+      visitCount: Number(visits.rows[0]?.sum),
+      shortenedUrls: usersUrls.rows,
+    };
 
-    const userDataWithNoLink = userData[0].shortenedUrls[0].id === null;
-    if (userDataWithNoLink) {
-      userData[0].shortenedUrls = [];
-    }
-
-    return res.status(STATUS_CODE.OK).send(userData[0]);
+    return res.send(myUrls);
   } catch (error) {
-    return res.sendStatus(STATUS_CODE.SERVER_ERROR);
+    return res.status(STATUS_CODE.SERVER_ERROR).send(error.message);
   }
 }
 
 async function getRanking(req, res) {
   try {
-    const { rows: ranking } = await userRepository.listRanking();
-    return res.status(STATUS_CODE.OK).send(ranking);
+    const ranking = await db.query(
+      'SELECT users.id, users.name, count(urls.userid) AS "linksCount", sum(urls.visitcount) AS visitcount FROM users LEFT JOIN urls ON urls.userid = users.id GROUP BY users.id ORDER BY visitcount desc LIMIT 10;'
+    );
+
+    return res.send(ranking.rows);
   } catch (error) {
+    console.log(error);
     return res.status(STATUS_CODE.SERVER_ERROR).send(error.message);
   }
 }
