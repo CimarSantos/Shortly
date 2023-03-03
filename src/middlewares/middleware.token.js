4c9b887393c3610bd680a48358df71daed8a93fd
CimarSantos <francimarlsantos@outlook.com>
2023-03-03 14:24:58 -0300

    Fix: Autenticação para o GET da rota /users/me

--- src/middlewares/middleware.token.js
+++ src/middlewares/middleware.token.js
@@ -1,4 +1,3 @@
-import jwt from "jsonwebtoken";
 import { STATUS_CODE } from "../enums/statusCode.js";
 import * as authRepository from "../repositories/authRepository.js";
 
@@ -14,36 +13,10 @@ async function urlTokenValidation(req, res, next) {
 
     res.locals.userId = isValidToken.userId;
   } catch (error) {
-    /* await authRepository.deleteUserFromSessions(token); */
-
     return res.status(STATUS_CODE.SERVER_ERROR).send(error.message);
   }
 
   next();
 }
 
-async function usersMeValidationToken(req, res, next) {
-  const { authorization } = req.headers;
-  const token = authorization?.replace("Bearer ", "");
-
-  try {
-    if (
-      !authorization ||
-      authorization === "" ||
-      typeof authorization !== "string"
-    )
-      return res.sendStatus(STATUS_CODE.UNAUTHORIZED);
-    const tokenExist = await authRepository.selectUserToken(token);
-
-    if (!tokenExist.rows[0]) return res.sendStatus(STATUS_CODE.NOT_FOUND);
-
-    res.locals.id = tokenExist.rows[0].userid;
-
-    next();
-  } catch (error) {
-    console.log(error);
-    return res.status(STATUS_CODE.SERVER_ERROR).send(error.message);
-  }
-}
-
-export { usersMeValidationToken, urlTokenValidation };
+export { urlTokenValidation };
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { getDataUsers, getRanking } from "../controllers/usersController.js";
-import { usersMeValidationToken } from "../middlewares/middleware.token.js";
+import { urlTokenValidation } from "../middlewares/middleware.token.js";
 
 const userRouter = Router();
 
-userRouter.get("/users/me", usersMeValidationToken, getDataUsers);
+userRouter.get("/users/me", urlTokenValidation, getDataUsers);
 userRouter.get("/ranking", getRanking);
 
 export default userRouter;
